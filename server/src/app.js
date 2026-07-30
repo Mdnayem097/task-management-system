9a6ff4b2c0f1d2e2ae846179a68086196712081f
Mdnayem097 <mdneloy097@gmail.com>
2026-07-30 22:19:39 +0600

    fix CORS issue

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Global Middlewares
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_URL,
   credentials: true
 }));
 app.use(express.json());
