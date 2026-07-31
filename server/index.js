949b62976e187564d6d573e6be3f76913106e308
Mdnayem097 <mdneloy097@gmail.com>
2026-07-31 21:43:11 +0600

    fix cors error

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
 
-const PORT = process.env.PORT || 5000;
+connectDB();
 
-connectDB().then(() => {
+module.exports = app;
+
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });
-});
+}
